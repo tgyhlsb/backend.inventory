7cabf7701fc77f17ef26d18d7d35ddbfc33780a4
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-09 11:57:49 +0200

    fixed models import in app.js

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // Connect to mongodb
@@ -23,6 +20,17 @@ connect();
 
 mongoose.connection.on('error', console.log);
 mongoose.connection.on('disconnected', connect);
+
+// Bootstrap models
+fs.readdirSync(__dirname + '/models').forEach(function (file) {
+  if (~file.indexOf('.js')) require(__dirname + '/models/' + file);
+});
+
+// Bootstrap passport config
+require('./config/passport')(passport, config);
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,19 +44,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 app.use('/', routes);
 app.use('/users', users);
-
-// Bootstrap models
-fs.readdirSync(__dirname + '/models').forEach(function (file) {
-  if (~file.indexOf('.js')) require(__dirname + '/models/' + file);
-});
-
-// Bootstrap passport config
-require('./config/passport')(passport, config);
-
-// Bootstrap application settings
-require('./config/express')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
