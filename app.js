8fcc991f40537ace1466ce7630d789edcfe0d22f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-09 10:46:20 +0200

    use new configs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
 var express = require('express.io');
 var path = require('path');
+var mongoose = require('mongoose');
+var config = require('config');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -9,6 +12,16 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Connect to mongodb
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  mongoose.connect(config.db, options);
+};
+connect();
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +37,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// Bootstrap models
+fs.readdirSync(__dirname + '/models').forEach(function (file) {
+  if (~file.indexOf('.js')) require(__dirname + '/models/' + file);
+});
+
+// Bootstrap passport config
+require('./config/passport')(passport, config);
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
