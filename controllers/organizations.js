1efc48ea6307c6e6792b1a0b520f7d21ea7401dd
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-10 17:12:37 +0200

    organizations' controller

---
+++ controllers/organizations.js
@@ -0,0 +1,63 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose');
+var Organization = mongoose.model('Organization');
+var utils = require('../lib/utils');
+
+/**
+ * Load
+ */
+
+exports.load = function (req, res, next) {
+  var organizationId = req.user.organization || req.params.organizationId;
+  if (!organizationId) return next(new Error('OrganizationId can\'t be null'));
+  var options = {
+    criteria: { _id : organizationId }
+  };
+  Organization.load(options, function (err, organization) {
+    if (err) return next(err);
+    if (!organization) return next(new Error('Failed to load Organization ' + organizationId));
+    req.organization = organization;
+    next();
+  });
+};
+
+/**
+ * Create organization
+ */
+
+exports.create = function (req, res) {
+  var organization = new Organization(req.body);
+  organization.owner = req.user;
+  organization.save(function (err) {
+    if (err) {
+      return res.status(400).json(err);
+    } else {
+      return res.status(200).json(organization);
+    }
+  });
+};
+
+/**
+ *  Show organization
+ */
+
+exports.showOne = function (req, res) {
+  var organization = req.organization;
+  if (organization) {
+    res.status(200).json(organization);
+  } else {
+    res.status(404).send('Organization not found');
+  }
+};
+
+/**
+ *  Show all organizations
+ */
+
+exports.showAll = function (req, res) {
+  res.json(['un', 'deux']);
+};
