38092addc83f2d2fbc434f87c69ed8a13db3be96
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-11 12:24:32 +0200

    removed duplicates and changed error display

--- app.js
+++ app.js
@@ -40,10 +40,11 @@ app.set('view engine', 'jade');
 // app.use(favicon(__dirname + '/public/favicon.ico'));
 // uncomment if not using Winston
 // app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// uncomment when not using configs of /config/express.js
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Models must be required after models
 var routes = require('./routes/index');
@@ -68,9 +69,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    res.json({
+      code: err.status,
+      error: err.message,
+      stack: err.stack
     });
   });
 }
@@ -79,9 +81,9 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  res.json({
+    code: res.status,
+    error: err.message
   });
 });
 
