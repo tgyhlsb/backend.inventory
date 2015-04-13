d641cdab2353b878cc2e9ce9433c102669547618
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-13 20:34:17 +0200

    removed unused code

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -41,7 +41,7 @@ exports.create = function (req, res, next) {
 };
 
 /**
- *  Set organization with admin role
+ *  Set user's organization with admin role
  */
 
 exports.setAdminOf = function (req, res, next) {
@@ -53,76 +53,10 @@ exports.setAdminOf = function (req, res, next) {
 };
 
 /**
- *  Show profile
- */
-
-exports.show = function (req, res) {
-  var user = req.profile;
-  res.render('users/show', {
-    title: user.name,
-    user: user
-  });
-};
-
-/**
- *  Show user
+ *  Show one profile
  */
 
 exports.showOne = function (req, res, next) {
   if (!req.profile) return next(new Error('Profile can\'t be null'));
   res.status(200).json(req.profile);
-};
-
-exports.signin = function (req, res) {};
-
-/**
- * Auth callback
- */
-
-exports.authCallback = login;
-
-/**
- * Show login form
- */
-
-exports.login = function (req, res) {
-  res.render('users/login', {
-    title: 'Login'
-  });
-};
-
-/**
- * Show sign up form
- */
-
-exports.signup = function (req, res) {
-  res.render('users/signup', {
-    title: 'Sign up',
-    user: new User()
-  });
-};
-
-/**
- * Logout
- */
-
-exports.logout = function (req, res) {
-  req.logout();
-  res.redirect('/login');
-};
-
-/**
- * Session
- */
-
-exports.session = login;
-
-/**
- * Login
- */
-
-function login (req, res) {
-  var redirectTo = req.session.returnTo ? req.session.returnTo : '/';
-  delete req.session.returnTo;
-  res.redirect(redirectTo);
 };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -8,14 +8,6 @@ var usersCtrl = require('usersCtrl');
 // Middlewares
 var auth = require('../config/middlewares/authorization');
 
-/* GET usersCtrl listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/login', usersCtrl.login);
-router.get('/signup', usersCtrl.signup);
-
 router
 .post('/',
   passport.authenticate('admin', { session: false }),
