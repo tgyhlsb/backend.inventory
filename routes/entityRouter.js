85b8482943e98d9bf8d35dfff5d9ab4a96e0597f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-20 21:29:22 +0200

    added entity router

---
+++ routes/entityRouter.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+// Controllers
+var entitiesCtrl = require('entitiesCtrl');
+
+// Middlewares
+var auth = require('../config/middlewares/authorization');
+
+router
+.get('/',
+  passport.authenticate('admin', { session: false }),
+  entitiesCtrl.fetch,
+  entitiesCtrl.showAll
+  )
+.get('/:userId',
+  passport.authenticate('admin', { session: false }),
+  entitiesCtrl.select,
+  entitiesCtrl.fetch,
+  entitiesCtrl.showOne
+  )
+.post('/',
+  passport.authenticate('admin', { session: false }),
+  entitiesCtrl.create,
+  entitiesCtrl.showOne
+  )
+.post('/query/',
+  passport.authenticate('admin', { session: false }),
+  entitiesCtrl.fetch,
+  entitiesCtrl.showAll
+  );
+
+
+module.exports = router;
