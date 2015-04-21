9193d509c96fe98fb862b728531be2902333a7dc
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-21 23:04:37 +0200

    added routes

--- routes/root.js
+++ routes/root.js
@@ -5,9 +5,11 @@ var passport = require('passport');
 // Routers
 var usersRoutes = require('./usersRouter');
 var organizationRoutes = require('./organizationRouter');
+var entityTypeRoutes = require('./entityTypeRouter');
 
 router.use('/users', usersRoutes);
 router.use('/organizations', organizationRoutes);
+router.use('/entityTypes', entityTypeRoutes);
 
 
 
