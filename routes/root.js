85326fb61b50f344a725d87457d3a516c803d228
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-26 18:00:26 +0200

    update routes

--- routes/root.js
+++ routes/root.js
@@ -7,11 +7,13 @@ var usersRoutes = require('./usersRouter');
 var organizationRoutes = require('./organizationRouter');
 var entityTypeRoutes = require('./entityTypeRouter');
 var entityRoutes = require('./entityRouter');
+var productTypeRoutes = require('./productTypeRouter');
 
 router.use('/users', usersRoutes);
 router.use('/organizations', organizationRoutes);
 router.use('/entityTypes', entityTypeRoutes);
 router.use('/entities', entityRoutes);
+router.use('/productTypes', productTypeRoutes);
 
 
 
